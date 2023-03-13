4421fc44f1cf67e45a22444f66c00ad422353b6c
matheushardman <matheus.hardman@gmail.com>
2023-03-13 07:03:56 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato("Rosildo", "Rosa", 5);
+        System.out.println(gato);
+        /*int a = 3;
+        int b = 5;
+        System.out.println("Hello World " + (a+b));*/
+    }
+
+}
